6d165c1943eeed83ee2abc0f75f9958eb63b568e
EternallyAscend <52477444+EternallyAscend@users.noreply.github.com>
2020-02-09 12:38:18 +0800

    Setup

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 latest="Version-3.0"
 if [ ! -d "/opt/scripts" ]; then
-    git checkout $latest
+    git clone https://github.com/hospital-ledger/scripts.git
 fi
+if [ -d "/opt/scripts" ]; then
+    result=$(git checkout $latest)
+    if [ "$result" == "Already on '$latest'" ]; then
+        echo -e "All right. Let's start"
+        else cd /opt
+        rm -f -r scripts
+        git clone https://github.com/hospital-ledger/scripts.git
+        cd /opt/scripts
+        git checkout $latest
+        echo -e "Well, all done."
+    fi
+fi
+cd /opt/scripts/
+chmod +777 ./*
+./first.sh
+./second.sh & ./download.sh
+./final.sh
